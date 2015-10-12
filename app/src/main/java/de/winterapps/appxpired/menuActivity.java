68ef14de7c31018a8c92379c9fff161ab7c6e50a
Shark919 <christoph.meise@gmx.de>
2015-10-12 10:24:06 +0200

    Initial commit.

---
+++ app/src/main/java/de/winterapps/appxpired/menuActivity.java
@@ -0,0 +1,13 @@
+package de.winterapps.appxpired;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
