8b107e10363ec311fff02fc83c6aaaca842f507d
Shark919 <christoph.meise@gmx.de>
2015-10-12 19:31:50 +0200

    second Activity

---
+++ app/src/main/java/de/winterapps/appxpired/addActivity.java
@@ -0,0 +1,18 @@
+package de.winterapps.appxpired;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Razor1996 on 12.10.2015.
+ */
+public class addActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
--- app/src/main/java/de/winterapps/appxpired/menuActivity.java
+++ app/src/main/java/de/winterapps/appxpired/menuActivity.java
@@ -1,7 +1,10 @@
 package de.winterapps.appxpired;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menuActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class menuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        final Button buttonAdd = (Button) findViewById(R.id.menuAddButton);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(menuActivity.this, addActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
