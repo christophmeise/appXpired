675d52ccf7dafc86f7dc8b01af016f61a93914ea
Shark919 <christoph.meise@gmx.de>
2015-10-12 10:35:36 +0200

    check

--- app/src/main/java/de/winterapps/appxpired/menuActivity.java
+++ app/src/main/java/de/winterapps/appxpired/menuActivity.java
@@ -9,5 +9,7 @@ public class menuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
     }
 }
