b2b3e27b25a8d72d6e08671852622eba897c00df
Alexander Friese <alexander.friese96@web.de>
2015-10-18 17:39:56 +0200

    intitial draft of login screen

---
+++ app/src/main/java/de/winterapps/appxpired/loginActivity.java
@@ -0,0 +1,17 @@
+package de.winterapps.appxpired;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by D062332 on 18.10.2015.
+ */
+public class loginActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_login);
+    }
+}
