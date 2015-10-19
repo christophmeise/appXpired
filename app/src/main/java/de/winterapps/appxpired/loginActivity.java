b20bcfe733256db56187029ed8f5cd3b004d80f2
Shark919 <christoph.meise@gmx.de>
2015-10-19 09:39:30 +0200

    Login screen desgin + start-up implementation

--- app/src/main/java/de/winterapps/appxpired/loginActivity.java
+++ app/src/main/java/de/winterapps/appxpired/loginActivity.java
@@ -1,8 +1,11 @@
 package de.winterapps.appxpired;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by D062332 on 18.10.2015.
@@ -13,5 +16,13 @@ public class loginActivity extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_login);
+
+        final Button buttonLogin = (Button) findViewById(R.id.loginLoginButton);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(loginActivity.this, menuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
