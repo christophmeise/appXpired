e07c1c4d5cd51b1c1fc046ed3391825aaf2a440e
DarthFriese <alexander.friese96@web.de>
2016-05-10 13:11:16 +0200

    settings screen and add template screen build

--- app/src/main/java/de/winterapps/appxpired/settingsActivity.java
+++ app/src/main/java/de/winterapps/appxpired/settingsActivity.java
@@ -1,9 +1,12 @@
 package de.winterapps.appxpired;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -18,17 +21,25 @@ public class settingsActivity extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_settings);
-        memberVariables members = memberVariables.sharedInstance;
+
+        final Button buttonAdd = (Button) findViewById(R.id.buttonTemplate);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(settingsActivity.this, templateActivity.class);
+                startActivity(intent);
+            }
+        });
+        /*memberVariables members = memberVariables.sharedInstance;
         currentHousehold = members.getHousehold();
         currentUsername = members.getUsername();
-        getLoginData();
+        getLoginData();*/
     }
 
-    public void getLoginData() {
+    /*public void getLoginData() {
         TextView userTextView = (TextView) findViewById(R.id.settingsTextViewUser);
         TextView householdTextView = (TextView) findViewById(R.id.settingsTextViewHousehold);
       //  Log.d("username:",currentUsername);
         userTextView.setText(currentUsername);
         householdTextView.setText(currentHousehold);
-    }
+    }*/
 }
---
+++ app/src/main/java/de/winterapps/appxpired/templateActivity.java
@@ -0,0 +1,16 @@
+package de.winterapps.appxpired;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by D062332 on 09.05.2016.
+ */
+public class templateActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_template);
+    }
+}
