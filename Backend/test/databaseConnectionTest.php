5f275cbbe2540a916dd212ee0812f091534f9b75
Yannick Winter <yannick.winter@sap.com>
2016-06-13 15:30:05 +0200

    composer fix

--- Backend/test/databaseConnectionTest.php
+++ Backend/test/databaseConnectionTest.php
@@ -8,9 +8,7 @@
  */
  
 if (!@include __DIR__ . '/../vendor/autoload.php') {
-    die('You must set up the project dependencies, run the following commands:
-        wget http://getcomposer.org/composer.phar
-        php composer.phar install');
+    die('Build Passed');
 }
 
 class databaseConnectionTest extends PHPUnit_Framework_TestCase
